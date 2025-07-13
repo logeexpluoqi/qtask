e40fd5d3ccb6c8109f8cc10f9175835db1787f1b
luoqi <50949439+logeexpluoqi@users.noreply.github.com>
2025-07-13 20:53:48 +0000

    refactor(qtask): update task scheduling macros and improve code clarity

--- qtask.c
+++ qtask.c
@@ -2,19 +2,29 @@
  * @Author: luoqi
  * @Date: 2021-04-29 19:27:09
  * @ Modified by: luoqi
- * @ Modified time: 2024-10-12 11:36
+ * @ Modified time: 2025-07-08 00:31
  */
 
+#include <stddef.h>
 #include "qtask.h"
 
-#define QTASK_LIST_ITERATOR(node, list)         for (node = (list)->next; node != (list); node = node->next)
-#define QTASK_LIST_ENTRY(ptr, type, member)     ((type *)((char *)(ptr) - ((unsigned long) &((type*)0)->member)))
+ // Macro for iterating through a doubly linked list
+#define QTASK_ITERATOR(node, list)  \
+    for (node = (list)->next; node != (list); node = node->next)
+
+#define QTASK_ITERATOR_SAFE(node, safe, list) \
+    for (node = (list) ? (list)->next : QNULL, safe = node ? node->next : QNULL; \
+         node && node != (list); \
+         node = safe, safe = node ? node->next : QNULL)
+
+// Macro for getting the pointer to the structure containing the doubly linked list node
+#define QTASK_ENTRY(ptr, type, member)  \
+   ((type *)((char *)(ptr) - ((size_t) &((type*)0)->member)))
 
 static inline void _list_insert(QTaskList *list, QTaskList *node)
 {
     list->next->prev = node;
     node->next = list->next;
-
     list->next = node;
     node->prev = list;
 }
@@ -23,37 +33,34 @@ static inline void _list_remove(QTaskList *node)
 {
     node->next->prev = node->prev;
     node->prev->next = node->next;
-
     node->next = node->prev = node;
 }
 
-static int _qtask_isexist(QTaskScheduler *sched, QTaskObj *task)
+static int _qtask_isexist(QTaskSched *sched, QTaskObj *task)
 {
     QTaskList *node;
     QTaskObj *_task;
 
-    QTASK_LIST_ITERATOR(node, &sched->sched_list) {
-        _task = QTASK_LIST_ENTRY(node, QTaskObj, task_node);
+    QTASK_ITERATOR(node, &sched->task_list)
+    {
+        _task = QTASK_ENTRY(node, QTaskObj, task_node);
         if(task->id == _task->id) {
             return 1;
-        } else {
-            continue;
         }
     }
     return 0;
 }
 
-static int _qdtask_isexsit(QTaskScheduler *sched, QTaskObj *task)
+static int _qdtask_isexsit(QTaskSched *sched, QTaskObj *task)
 {
     QTaskList *node;
     QTaskObj *_task;
 
-    QTASK_LIST_ITERATOR(node, &sched->unsched_list) {
-        _task = QTASK_LIST_ENTRY(node, QTaskObj, task_node);
+    QTASK_ITERATOR(node, &sched->suspend_list)
+    {
+        _task = QTASK_ENTRY(node, QTaskObj, task_node);
         if(task->id == _task->id) {
             return 1;
-        } else {
-            continue;
         }
     }
     return 0;
@@ -61,147 +68,201 @@ static int _qdtask_isexsit(QTaskScheduler *sched, QTaskObj *task)
 
 static uint16_t _id_calc(const char *name)
 {
-    uint16_t id = 0;
-    if(name == QTASK_NONE) {
+    if(!name) {
         return 0;
     }
-    for(int i = 0; name[i] != '\0'; i++) {
-        id += (name[i] + i);
+    uint16_t hash = 5381;
+    int c;
+    while((c = *name++)) {
+        hash = (hash << 5) + hash + c; // hash * 33 + c
     }
-    return id;
+    return hash;
 }
 
-void qtask_scheduler_init(QTaskScheduler *sched)
+void qtask_sched_init(QTaskSched *sched)
 {
-    sched->sched_list.prev = sched->sched_list.next = &sched->sched_list;
-    sched->unsched_list.prev = sched->unsched_list.next = &sched->unsched_list;
+    sched->task_list.prev = sched->task_list.next = &sched->task_list;
+    sched->suspend_list.prev = sched->suspend_list.next = &sched->suspend_list;
 }
 
-int qtask_attach(QTaskScheduler *sched, QTaskObj *task, const char *name, QTaskHandle handle, uint32_t tick, const char *usage)
+int qtask_add(QTaskSched *sched, QTaskObj *task, const char *name, QTaskHandle handle, size_t tick)
 {
     task->name = name;
     task->id = _id_calc(name);
     task->isready = 0;
     task->handle = handle;
     task->timer = tick;
-    task->tick = tick;
-    task->usage = usage;
+    task->period = tick;
     task->rtime = 0;
     task->rtick = 0;
+
     if(_qdtask_isexsit(sched, task)) {
         task->isready = 0;
-        task->timer = task->tick;
+        task->timer = task->period;
         _list_remove(&task->task_node);
     }
 
     if(!_qtask_isexist(sched, task)) {
-        _list_insert(&sched->sched_list, &task->task_node);
+        _list_insert(&sched->task_list, &task->task_node);
         return 0;
     }
     return 1;
 }
 
-int qtask_detach(QTaskScheduler *sched, QTaskObj *task)
+int qtask_del(QTaskSched *sched, QTaskObj *task)
 {
     if(_qtask_isexist(sched, task)) {
         task->isready = 0;
-        task->timer = task->tick;
+        task->timer = task->period;
         _list_remove(&task->task_node);
     }
 
     if(!_qdtask_isexsit(sched, task)) {
-        _list_insert(&sched->unsched_list, &task->task_node);
+        _list_insert(&sched->suspend_list, &task->task_node);
         return 0;
-    } else {
-        return -1;
     }
+    return -1;
 }
 
-QTaskObj *qtask_get(QTaskScheduler *sched, const char *taskname)
+int qtask_suspend(QTaskSched *sched, const char *name)
+{
+    QTaskList *node, *safe;
+    QTaskObj *task;
+    uint16_t id = _id_calc(name);
+
+    QTASK_ITERATOR_SAFE(node, safe, &sched->task_list)
+    {
+        task = QTASK_ENTRY(node, QTaskObj, task_node);
+        if(task->id == id) {
+            if(_qtask_isexist(sched, task)) {
+                task->isready = 0;
+                task->timer = task->period;
+                _list_remove(&task->task_node);
+            }
+
+            if(!_qdtask_isexsit(sched, task)) {
+                _list_insert(&sched->suspend_list, &task->task_node);
+                return 0;
+            }
+            return -1;
+        }
+    }
+    return -1;
+
+}
+
+int qtask_resume(QTaskSched *sched, const char *name)
+{
+    QTaskList *node, *safe;
+    QTaskObj *task;
+    uint16_t id = _id_calc(name);
+
+    QTASK_ITERATOR_SAFE(node, safe, &sched->suspend_list)
+    {
+        task = QTASK_ENTRY(node, QTaskObj, task_node);
+        if(task->id == id) {
+            if(_qdtask_isexsit(sched, task)) {
+                task->isready = 0;
+                task->timer = task->period;
+                _list_remove(&task->task_node);
+            }
+
+            if(!_qtask_isexist(sched, task)) {
+                _list_insert(&sched->task_list, &task->task_node);
+                return 0;
+            }
+        }
+    }
+    return -1;
+}
+
+QTaskObj *qtask_obj(QTaskSched *sched, const char *taskname)
 {
     QTaskList *node;
     QTaskObj *task;
     uint16_t id = _id_calc(taskname);
-    QTASK_LIST_ITERATOR(node, &sched->sched_list) {
-        task = QTASK_LIST_ENTRY(node, QTaskObj, task_node);
+
+    QTASK_ITERATOR(node, &sched->task_list)
+    {
+        task = QTASK_ENTRY(node, QTaskObj, task_node);
         if(task->id == id) {
             return task;
-        } else {
-            continue;
         }
     }
-    return QTASK_NONE;
+    return QNULL;
 }
 
-void qtask_exec(QTaskScheduler *sched)
+void qtask_exec(QTaskSched *sched)
 {
-    QTaskList *node;
+    QTaskList *node, *safe;
     QTaskObj *task;
-    void *args;
-    QTASK_LIST_ITERATOR(node, &sched->sched_list) {
-        task = QTASK_LIST_ENTRY(node, QTaskObj, task_node);
+
+    QTASK_ITERATOR_SAFE(node, safe, &sched->task_list)
+    {
+        task = QTASK_ENTRY(node, QTaskObj, task_node);
         if(task->isready) {
-            sched->run_task = task;
-            if((sched->targetid == task->id) || (sched->targetid == 0)) {
-                args = sched->args;
-            } else {
-                args = QTASK_NONE;
-            }
-            task->handle(args);
-            task->isready = 0;
+            task->handle();
             task->rtime = task->rtick;
+            task->isready = 0;
             task->rtick = 0;
-            if(sched->targetid != 0) {
-                sched->args = QTASK_NONE;
-            }
         }
     }
 }
 
-void qtask_param_set(QTaskScheduler *sched, const char *taskname, void *args)
+void qtask_tick_increase(QTaskSched *sched)
 {
-    sched->targetid = _id_calc(taskname);
-    sched->args = args;
-}
-
-void qtask_tick_increase(QTaskScheduler *sched)
-{
-    QTaskList *node;
+    QTaskList *node, *safe;
     QTaskObj *task;
+    int count = 0;
 
-    QTASK_LIST_ITERATOR(node, &sched->sched_list) {
-        task = QTASK_LIST_ENTRY(node, QTaskObj, task_node);
+    QTASK_ITERATOR_SAFE(node, safe, &sched->task_list)
+    {
+        if(node == QNULL || node->next == QNULL || node->prev == QNULL) {
+            return;
+        }
+
+        task = QTASK_ENTRY(node, QTaskObj, task_node);
+        if(task == QNULL) {
+            return;
+        }
+
         if(task->timer > 0) {
-            task->timer--;
-            if(task->timer == 0) {
+            if(--task->timer <= 0) {
                 task->isready = 1;
-                task->timer = task->tick;
+                sched->run_task = task;
+                task->timer = task->period;
             }
+        }
+        count++;
+
+        if(count > 1000) {
+            return;
         }
     }
 }
 
-void qtask_runtime_increase(QTaskScheduler *sched)
+void qtask_runtime_increase(QTaskSched *sched)
 {
     QTaskList *node;
     QTaskObj *task;
 
-    QTASK_LIST_ITERATOR(node, &sched->sched_list) {
-        task = QTASK_LIST_ENTRY(node, QTaskObj, task_node);
+    QTASK_ITERATOR(node, &sched->task_list)
+    {
+        task = QTASK_ENTRY(node, QTaskObj, task_node);
         if(task->isready) {
             task->rtick++;
         }
     }
 }
 
-void qtask_sleep(QTaskScheduler *sched, uint32_t tick)
+void qtask_sleep(QTaskSched *sched, size_t tick)
 {
-    if(sched->run_task != QTASK_NONE) {
+    if(!sched->run_task) {
         sched->run_task->timer = tick;
     }
 }
 
-void qtask_tick_set(QTaskObj *obj, uint32_t tick)
+void qtask_tick_set(QTaskObj *obj, size_t tick)
 {
-    obj->tick = tick;
+    obj->period = tick;
 }
--- qtask.h
+++ qtask.h
@@ -2,7 +2,7 @@
  * @Author: luoqi 
  * @Date: 2021-04-29 19:27:49 
  * @ Modified by: luoqi
- * @ Modified time: 2024-10-12 11:39
+ * @ Modified time: 2025-07-08 00:31
  */
 
 #ifndef _QTASK_H
@@ -13,73 +13,182 @@
 #endif
 
 #include <stdint.h>
+#include <stddef.h>
 
-#define QTASK_NONE ((void*)0)
+#ifndef QNULL
+#define QNULL ((void *)0)
+#endif
 
+/**
+ * @struct QTaskList
+ * @brief Represents a node in a doubly linked list.
+ * 
+ * This structure is used to implement a doubly linked list for task scheduling.
+ */
 typedef struct _task_list {
-    struct _task_list* prev;
-    struct _task_list* next;
+    struct _task_list* prev; /**< Pointer to the previous node in the list. */
+    struct _task_list* next; /**< Pointer to the next node in the list. */
 } QTaskList;
 
+/**
+ * @struct QTaskObj
+ * @brief Represents a task object.
+ * 
+ * This structure encapsulates all the information related to a task, including its name, ID,
+ * execution function, timing information, and usage description.
+ */
 typedef struct _qtask
 {
-    const char* name;
-    uint32_t id : 30;
-    uint32_t isready : 1;
-    void (*handle)(void *args);
-    uint32_t timer;
-    uint32_t tick;
-    uint32_t rtime;
-    uint32_t rtick;
-    QTaskList task_node;
-    const char* usage;
+    const char* name;       /**< Name of the task. */
+    uint16_t id;            /**< Unique identifier of the task */
+    uint8_t isready;        /**< Flag indicating whether the task is ready to execute. */
+    void (*handle)(void); /**< Function pointer to the task's execution function. */
+    size_t timer;         /**< Timer value for the task, counting down to execution. */
+    size_t period;          /**< Periodic tick value for the task. */
+    size_t rtime;         /**< Recorded execution time of the task. */
+    size_t rtick;         /**< Running tick count of the task. */
+    QTaskList task_node;    /**< Doubly linked list node for task scheduling. */
 } QTaskObj;
-typedef void (*QTaskHandle)(void *args);
 
+/**
+ * @typedef QTaskHandle
+ * @brief Function pointer type for task execution functions.
+ * 
+ * Task execution functions should accept a void pointer as an argument.
+ */
+typedef void (*QTaskHandle)(void);
+
+/**
+ * @struct QTaskSched
+ * @brief Represents a task scheduler.
+ * 
+ * This structure manages the scheduling of tasks, including task arguments, target task ID,
+ * the currently running task, and two doubly linked lists for scheduled and unscheduled tasks.
+ */
 typedef struct
 {
-    void *args;
-    uint32_t targetid;
-    QTaskObj *run_task;
-    QTaskList sched_list;
-    QTaskList unsched_list;
-} QTaskScheduler;
+    void *args;             /**< Arguments to be passed to the task. */
+    QTaskObj *run_task;     /**< Pointer to the currently running task. */
+    QTaskList task_list;   /**< Doubly linked list for scheduled tasks. */
+    QTaskList suspend_list; /**< Doubly linked list for unscheduled tasks. */
+} QTaskSched;
 
-
-void qtask_scheduler_init(QTaskScheduler *sched);
-
-int qtask_attach(QTaskScheduler *sched, QTaskObj* task, const char* name, QTaskHandle handle, uint32_t tick, const char* usage);
-
-int qtask_detach(QTaskScheduler *sched, QTaskObj* task);
-
-void qtask_exec(QTaskScheduler *sched);
-
-/** 
- * If task name is NULL, the args will be set to all tasks, 
- * and the args will be executed by all tasks periodially, till parameter is set to a task.
- * If task name is not NULL, the args will be set to a specific task, and only executed once.
+/**
+ * @brief Initializes the task scheduler.
+ * 
+ * This function initializes the scheduled and unscheduled task lists of the task scheduler.
+ * 
+ * @param sched Pointer to the task scheduler object.
  */
-void qtask_param_set(QTaskScheduler *sched, const char *taskname, void *args);
+void qtask_sched_init(QTaskSched *sched);
 
-QTaskObj *qtask_get(QTaskScheduler *sched, const char *taskname);
-
-void qtask_tick_increase(QTaskScheduler *sched);
-
-/** 
- * It is used to measure the task callback function execution time.
- * It must be called in a timer interrupt function faster than qtask_tick_increase.
+/**
+ * @brief Adds a task to the task scheduler.
+ * 
+ * This function initializes a task object and adds it to the scheduled task list if it's not already there.
+ * If the task exists in the unscheduled list, it will be removed first.
+ * 
+ * @param sched Pointer to the task scheduler object.
+ * @param task Pointer to the task object to be added.
+ * @param name Name of the task.
+ * @param handle Function pointer to the task's execution function.
+ * @param tick Periodic tick value for the task.
+ * @return 0 if the task is successfully added, 1 if the task already exists in the scheduled list.
  */
-void qtask_runtime_increase(QTaskScheduler *sched);
+int qtask_add(QTaskSched *sched, QTaskObj* task, const char* name, QTaskHandle handle, size_t tick);
 
-/** 
- * Change task periodic time onece 
+/**
+ * @brief Removes a task from the task scheduler.
+ * 
+ * This function removes the task from the scheduled list if it exists and adds it to the unscheduled list.
+ * 
+ * @param sched Pointer to the task scheduler object.
+ * @param task Pointer to the task object to be removed.
+ * @return 0 if the task is successfully removed and added to the unscheduled list, -1 otherwise.
  */
-void qtask_sleep(QTaskScheduler *sched, uint32_t tick);
+int qtask_del(QTaskSched *sched, QTaskObj* task);
 
-/** 
- * Change task periodic time 
+/**
+ * @brief Schedules a task for execution.
+ * 
+ * This function adds the task to the scheduled list if it is not already scheduled.
+ * 
+ * @param sched Pointer to the task scheduler object.
+ * @param task Pointer to the task object to be scheduled.
+ * @return 0 if the task is successfully added to the scheduled list, -1 otherwise.
  */
-void qtask_tick_set(QTaskObj* task, uint32_t tick);
+int qtask_suspend(QTaskSched *sched, const char *name);
+
+/**
+ * @brief Resumes a suspended task.
+ * 
+ * This function resumes the specified task by removing it from the suspended list.
+ * 
+ * @param sched Pointer to the task scheduler object.
+ * @param task Pointer to the task object to be resumed.
+ * @return 0 if the task is successfully resumed, -1 otherwise.
+ */
+int qtask_resume(QTaskSched *sched, const char *name);
+
+/**
+ * @brief Executes all ready tasks in the task scheduler.
+ * 
+ * This function iterates through the scheduled task list and executes all tasks marked as ready.
+ * 
+ * @param sched Pointer to the task scheduler object.
+ */
+void qtask_exec(QTaskSched *sched);
+
+/**
+ * @brief Retrieves a task object by its name.
+ * 
+ * This function searches the scheduled task list for a task with the specified name.
+ * 
+ * @param sched Pointer to the task scheduler object.
+ * @param taskname Name of the task to retrieve.
+ * @return Pointer to the task object if found, QTASK_NONE otherwise.
+ */
+QTaskObj *qtask_obj(QTaskSched *sched, const char *taskname);
+
+/**
+ * @brief Increases the timer count of all tasks in the task scheduler.
+ * 
+ * This function should be called periodically to update the timer values of all tasks.
+ * When a task's timer reaches zero, it will be marked as ready for execution.
+ * 
+ * @param sched Pointer to the task scheduler object.
+ */
+void qtask_tick_increase(QTaskSched *sched);
+
+/**
+ * @brief Measures the execution time of tasks.
+ * 
+ * This function should be called in a timer interrupt function with a higher frequency than
+ * qtask_tick_increase to measure the execution time of task callback functions.
+ * 
+ * @param sched Pointer to the task scheduler object.
+ */
+void qtask_runtime_increase(QTaskSched *sched);
+
+/**
+ * @brief Changes the periodic time of a running task.
+ * 
+ * This function changes the timer value of the currently running task.
+ * 
+ * @param sched Pointer to the task scheduler object.
+ * @param tick New periodic tick value for the task.
+ */
+void qtask_sleep(QTaskSched *sched, size_t tick);
+
+/**
+ * @brief Changes the periodic time of a task.
+ * 
+ * This function changes the periodic tick value of a specified task.
+ * 
+ * @param task Pointer to the task object.
+ * @param tick New periodic tick value for the task.
+ */
+void qtask_tick_set(QTaskObj *task, size_t tick);
 
 #ifdef __cplusplus
  }
